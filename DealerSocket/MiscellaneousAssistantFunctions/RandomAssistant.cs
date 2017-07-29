6f663aa8b0fbea3429dffc8333de4196164e30da
Patrick Conner <PConner000@gmail.com>
2017-07-29 01:09:23 -0600

    Added ASP project for future development. Added MiscellaneousAssistantFunctions project for various miscellaneous functions that may need written      Added getting a random base 64 number of N digits (defaulted to 6) as a string for creating unique IDs.

---
+++ DealerSocket/MiscellaneousAssistantFunctions/RandomAssistant.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Immutable;
+
+namespace NWA.HustleCards.Misc
+{
+    public static class RandomAssistant
+    {
+        private static Random rand = new Random();
+        private static readonly ImmutableArray<char> base64Digits = new ImmutableArray<char>()
+        {
+            '0',
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6',
+            '7',
+            '8',
+            '9',
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z',
+            'A',
+            'B',
+            'C',
+            'D',
+            'E',
+            'F',
+            'G',
+            'H',
+            'I',
+            'J',
+            'K',
+            'L',
+            'M',
+            'N',
+            'O',
+            'P',
+            'Q',
+            'R',
+            'S',
+            'T',
+            'U',
+            'V',
+            'W',
+            'X',
+            'Y',
+            'Z',
+            '_',
+            '-',
+        };
+
+        public static string RandBase64(int digits = 6)
+        {
+            string ret = "";
+            for(int i = 0; i < digits; i++)
+            {
+                ret += base64Digits[rand.Next(base64Digits.Length)];
+            }
+            return ret;
+        }
+    }
+}
