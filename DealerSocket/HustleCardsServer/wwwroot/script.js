7adf176a3ee9db5d759832f262f0934587b47b1e
DShoupe <dylanshoupe@yahoo.com>
2017-07-29 22:20:00 -0600

    Added javascript to some html for logic

--- DealerSocket/HustleCardsServer/wwwroot/script.js
+++ DealerSocket/HustleCardsServer/wwwroot/script.js
@@ -1,3 +1,9 @@
-document.getElementById("loginBtn").addEventListener("click", function () {
-  var jobValue = document.getElementsByName('username')[0].value;
-});
+
+function test(){
+	document.write(document.getElementById("username").value);
+}
+
+document.getElementById("loginBtn").addEventListener("click", function printTest() {
+	var jobValue = document.getElementsByName('username')[0].value;
+	document.write(jobValue);
+	});
